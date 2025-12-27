0f9634a8d8febf8398e0e41b689dc82e498c60c3
Vinaytitarmare <146834025+Vinaytitarmare@users.noreply.github.com>
2025-12-28 00:44:46 +0530

    render fixed

--- app/metro.config.js
+++ app/metro.config.js
@@ -4,6 +4,8 @@ const { getDefaultConfig } = require('expo/metro-config');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
+config.resolver.sourceExts.push('mjs');
+
 
 
 config.resolver.extraNodeModules = {
